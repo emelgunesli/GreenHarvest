531f9c814289f0cb549973752374490092200bbf
aylin <4ylingunes@gmail.com>
2026-02-25 13:43:50 +0100

    add vegetables and advertisement responsive css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import "./css/styles.css";
-
+import './css/styles.css'
+import './css/tablet.css'
+import './css/mobil.css'
 // If you have JS logic later, write it here.
 console.log("GreenHarvest main.js loaded");
